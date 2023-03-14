6d018b7d1c6c26553927aaee23ea220f7bc4f87b
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-14 22:24:26 +0100

    edited fetch url

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
-const COUNTRIES_API_URL = 'https://restcountries.com/#api-endpoints-v3-name';
+const fetchCountries = fetch(
+  'https://restcountries.com/#api-endpoints-v3-name'
+);
 
 const form = document.getElementById('#search-box');
 
-function fetchCountries(name) {
-  fetch(COUNTRIES_API_URL)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-    });
-}
+fetchCountries
+  .then(response => {
+    const countriesData = response.json();
+    console.log(countriesData);
+    return countriesData;
+  })
+  .then(countries => {
+    console.log(countries);
+  })
+  .catch(error => {
+    console.log(error);
+  });
