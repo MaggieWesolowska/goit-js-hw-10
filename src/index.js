528fb9e89d70317838ee97c9dbf4e90bdef90fbb
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-14 22:10:52 +0100

    edited js added lodash library

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const COUNTRIES_API_URL = 'https://restcountries.com/#api-endpoints-v3-name';
+
+const form = document.getElementById('#search-box');
+
+function fetchCountries(name) {
+  fetch(COUNTRIES_API_URL)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    });
+}
