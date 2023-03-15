bb6a9b5887040b895d4b49340ee3f5b94d48fab6
MaggieWesolowska <114258920+MaggieWesolowska@users.noreply.github.com>
2023-03-15 14:53:57 +0000

    Deploying to gh-pages from @ MaggieWesolowska/goit-js-hw-10@148ea8a9a8ce62ef2a75e01601384a5011004caf 🚀

--- index.366ef5b0.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("#search-box"),document.querySelector(".country-list"),document.querySelector(".country-info");
-//# sourceMappingURL=index.366ef5b0.js.map
--- index.b787c3b2.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("#search-box"),document.querySelector(".country-list"),document.querySelector(".country-info");
-//# sourceMappingURL=index.b787c3b2.js.map
