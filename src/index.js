148ea8a9a8ce62ef2a75e01601384a5011004caf
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-15 15:53:32 +0100

    import Notiflix and debounce

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
