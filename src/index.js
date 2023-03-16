e0a9ea5253065597992145e283faedd94b6edfd1
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-16 19:55:13 +0100

    added eventListener

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const countryInfo = document.querySelector('.country-info');
 countryInput.addEventListener(
   'input',
   debounce(event, DEBOUNCE_DELAY => {
-    fetchCountries(name);
+    fetchCountries();
   })
 );
 
@@ -21,10 +21,14 @@ const fetchCountries = name => {
     'https://restcountries.com/v3.1/name/' +
     countryInput.value.trim() +
     '?fields=name,capital,population,flags,languages';
-  return fetch(countryName).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    response.json();
-  });
+  return fetch(countryName)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      response.json();
+    })
+    .catch(error => {
+      console.log(error);
+    });
 };
