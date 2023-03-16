55a40518e66cae4e206794b929e506555df48b7a
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-16 20:12:43 +0100

    added render function

--- src/index.js
+++ src/index.js
@@ -9,26 +9,36 @@ const countryInput = document.getElementById('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-countryInput.addEventListener(
-  'input',
-  debounce(event, DEBOUNCE_DELAY => {
-    fetchCountries();
-  })
-);
+countryInput.addEventListener('input', () => {
+  fetchCountries()
+    .then(countries => renderCountry(countries))
+    .catch(error => console.log(error));
+});
 
-const fetchCountries = name => {
+function fetchCountries(name) {
   const countryName =
     'https://restcountries.com/v3.1/name/' +
     countryInput.value.trim() +
     '?fields=name,capital,population,flags,languages';
-  return fetch(countryName)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      response.json();
+  return fetch(countryName).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    response.json();
+  });
+}
+
+function renderCountry() {
+  const markup = countries
+    .map(country => {
+      return `<div class="country-card">
+          <p>${country.name.official}</p>
+          <p>${country.capital}</p>
+          <p>${country.population}</p>
+          <p>${country.flags}</p>
+          <p>${country.languages}</p>
+        </div>`;
     })
-    .catch(error => {
-      console.log(error);
-    });
-};
+    .join('');
+  countryInfo.innerHTML = markup;
+}
